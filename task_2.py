4541d6c525adf04319b42bfe8d70271a622e16d7
jesus-333 <albi25.zanca@gmail.com>
2023-08-19 16:59:54 +0200

    Bug correction

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ from library import game_engine
 def main():
     game = game_engine.Game('data/pokemon_2.json', 'data/moves_2.json')
 
-    game.play()
+    game.play_story()
 
 if __name__ == '__main__':
     main()
