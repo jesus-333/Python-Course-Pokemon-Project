0b1ca483688c86e83dc5f9c8a92d5c2f90f73912
jesus-333 <albi25.zanca@gmail.com>
2023-08-27 06:02:00 +0200

    Work on random battle

--- library/random_engine.py
+++ library/random_engine.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 from . import game_engine, Battle, Pokemon
 
@@ -16,9 +17,33 @@ class random_game_engin(game_engine):
         self.print_var = False
 
     def simulate_battles(self):
+        # Variable to save statistics 
+        wild_pokemon_encountered = {}
+        outcome_counter = dict( win = 0, loss = 0)
+        turns_per_battle = np.zeros(self.battle_to_simulate)
+        percentage_hp_after_battle = np.zeros(self.battle_to_simulate)
+
         for i in range(self.battle_to_simulate):
-            battle = RandomBattle()
-            battle_outcome = battle.execute_battle()
+            # Spawn and fight wild pokemon
+            wild_pokemon = self.get_wild_pokemon()
+            battle = RandomBattle(self.trainer.pokemon_list[0], wild_pokemon)
+            battle_outcome, turns = battle.execute_battle()
+
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+            # Save statistics
+            
+            if wild_pokemon.name in wild_pokemon_encountered:
+                wild_pokemon_encountered[wild_pokemon.name] += 1
+            else:
+                wild_pokemon_encountered[wild_pokemon.name] = 0
+
+            if battle_outcome == 1: outcome_counter['win'] += 1
+            elif battle_outcome == 2: outcome_counter['loss'] += 1
+
+            turns_per_battle[i] = turns
+            percentage_hp_after_battle[i] = self.trainer.pokemon_list[0].base_stats['hp'] /  self.trainer.pokemon_list[0].base_stats['max_hp']
+
+        return wild_pokemon_encountered, outcome_counter, turns_per_battle, percentage_hp_after_battle
 
     def clean_moves(self):
         """
@@ -29,7 +54,7 @@ class random_game_engin(game_engine):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     # Spawn pokemon methods
 
-    def get_wild_pokemon(self,):
+    def get_wild_pokemon(self):
         """
         Create a random wild pokemon
         """
@@ -68,6 +93,7 @@ class RandomBattle(Battle):
 
     def execute_battle(self):
         continue_battle = True
+        turns = 0
         while continue_battle:
             # Select a random move for pokemon 1
             moves_1 = self.pokemon_1.moves
@@ -80,8 +106,12 @@ class RandomBattle(Battle):
             outcome = self.execute_both_move(idx_move_1, idx_move_2,)
             exit_status_battle, continue_battle = self.eveluate_battle_outcome(outcome)
 
-        return exit_status_battle
+            self.recharge_pp(self.pokemon_1)
+            self.recharge_pp(self.pokemon_2)
 
+            turn += 1
 
-    def recharge_pp(self):
-        pass
+        return exit_status_battle, turn
+
+    def recharge_pp(self, pokemon : "Pokemon"):
+        for move in pokemon.moves: move.pp = move.max_pp
