150b0d83b0e0a0357ce0f38025653c44293147ab
jesus-333 <albi25.zanca@gmail.com>
2023-08-29 15:15:04 +0200

    Work on task 4

--- library/plot_task_4.py
+++ library/plot_task_4.py
@@ -3,26 +3,34 @@ import matplotlib.pyplot as plt
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-def plot_health_during_fight(starter_list : list, battle_statistcs_per_starter : dict, max_number_of_turns : int):
+def plot_health_during_fight(starter_list : list, battle_statistcs_per_starter : dict, max_number_of_turns_per_starter : dict):
+    fig, ax = plt.subplots(1, 1, figsize = (12, 8))
+
     for starter in starter_list:
+        max_number_of_turns = max_number_of_turns_per_starter[starter]
+
         # Get the list of hp during battle
         battle_statistcs = battle_statistcs_per_starter[starter]
         tmp_hp_during_battle = battle_statistcs['hp_during_battle']
         hp_during_battle = np.zeros((len(tmp_hp_during_battle), max_number_of_turns + 1)) # The +1 is present because in position 0 I always save the hp before the battle. Note that the hp are saved after the end of each turn
 
         for i in range(len(tmp_hp_during_battle)):
-            hp_during_battle[i, 0:len(tmp_hp_during_battle[i])] = tmp_hp_during_battle[i]
+            hp_during_battle[i, 0:len(tmp_hp_during_battle[i])] = tmp_hp_during_battle[i] / tmp_hp_during_battle[i][0]
 
         average_hp_during_battle = hp_during_battle.mean(0)
         std_hp_during_battle = hp_during_battle.std(0)
         turns = np.arange(max_number_of_turns + 1)
 
-        fig, ax = plt.subplots(1, 1, figsize = (15, 10))
 
-        ax.plot(turns, average_hp_during_battle)
+        ax.plot(turns, average_hp_during_battle, label = starter)
+        ax.fill_between(turns, average_hp_during_battle + std_hp_during_battle, average_hp_during_battle - std_hp_during_battle, alpha = 0.2)
 
-        ax.set_title("Average %HP after battle")
-
-        fig.tight_layout()
-        fig.show()
+    ax.set_title("Average %HP after battle")
+    ax.legend()
+    ax.grid(True)
+    ax.set_ylabel("% Health")
+    ax.set_xlabel("N. Turns")
+    ax.set_xlim([turns[0], turns[-1]])
+    fig.tight_layout()
+    fig.show()
                
---
+++ task_4_plot.py
@@ -0,0 +1,37 @@
+import sys
+import pickle
+import numpy as np
+
+from library import  plot_task_4
+
+def load_data(starter_list : int, file_name : str, n_games : int, n_battles : int) -> dict:
+    outcome_counter = dict()
+    for starter in starter_list:
+        pickle_in = open("results/{}_{}_{}_{}.pickle".format(file_name, starter, n_games, n_battles), "rb")
+        outcome_counter_starter = pickle.load(pickle_in)
+        outcome_counter[starter] = outcome_counter_starter
+        pickle_in.close()
+
+    return outcome_counter
+
+def print_statistics(statistics):
+    print("\t\tMean           = ", np.mean(statistics))
+    print("\t\tMedian         = ", np.median(statistics))
+    print("\t\t25th quartile = ", np.quantile(statistics, 0.25))
+    print("\t\t75th quartile = ", np.quantile(statistics, 0.75))
+
+if __name__ == '__main__':
+    n_games = int( sys.argv[1] )
+    n_battles = int( sys.argv[2] )
+
+    starter_list = ["pikachu", "bulbasaur", "charmander", "squirtle"]
+    
+    outcome_counter = load_data(starter_list, "outcome_counter", n_games, n_battles)
+    turns_per_battle = load_data(starter_list, "turns_per_battle", n_games, n_battles)
+    percentage_hp_after_battle = load_data(starter_list, "percentage_hp_after_battle", n_games, n_battles)
+    wild_pokemon_encountered = load_data(starter_list, "wild_pokemon_encountered", n_games, n_battles) 
+    battle_statistics = load_data(starter_list, "battle_statistics", n_games, n_battles)
+    
+    max_number_of_turns_per_starter = dict()
+    for starter in starter_list: max_number_of_turns_per_starter[starter] = int(turns_per_battle[starter].max())
+    plot_task_4.plot_health_during_fight(starter_list , battle_statistics , max_number_of_turns_per_starter)
